b6244c31d4445479225e03b6cae3bcd33c7cac84
Hamit Mizrak <hamitmizrak@gmail.com>
2025-01-14 15:08:15 +0300

    Math

---
+++ _05_math.py
@@ -0,0 +1,32 @@
+################################################################
+# Math
+import math
+from random import random
+
+################################################################
+
+print("PI: " ,math.pi)
+print("E: " ,math.e)
+
+print("Karekök: ",math.sqrt(16))
+print("üslü: " ,math.pow(2,5))
+print("ceil: " ,math.ceil(9.1))
+print("floor: " ,math.floor(9.9))
+
+number1=[1,2,3,4,5]
+print("Sum:", math.sum(number1))
+print("Average:", math.sum(number1)/ len(number1))
+print("Max:", math.max(number1))
+print("Min:", math.min(number1))
+
+################################################################
+print("Logaritma: " , str(math.log(100)))
+print("Sinüs: " , str(math.sin(math.radians(90))))
+print("Kosinüs: " , str(math.cos(math.radians(90))))
+print("Trigonometrik açıların radyan cevresi: " , str(math.radians(90)))
+print("Trigonometrik açıların derece cevresi: " , str(math.degrees(math.pi/2)))
+################################################################
+# Rastgele
+# print("Rastgeler Tam sayı 1-50 arasında", random.randint(1,50))
+# print("Rastgeler Ondalık sayı", random.uniform(0,1))
+
