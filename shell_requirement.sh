8af1472bf7738e8317cca72d24eda92b0231bac1
Hamit Mizrak <hamitmizrak@gmail.com>
2025-01-15 00:00:48 +0300

    revize

--- shell_requirement.sh
+++ shell_requirement.sh
@@ -4,6 +4,36 @@ chmod +x shell_countdown.sh
 
 
 ################################################################
+
+#  Hangi Yükleme Türünü Kullanmalısınız?
+# Global yükleme:
+#  Sistem genelinde kullanılan araçlar ve kütüphaneler için (örneğin, black, pylint gibi araçlar).
+#  Yerel yükleme (local):
+# Bir proje için bağımlılıkları izole etmek için. Bu en iyi uygulamadır.
+
+
+# Genel Kütüphane
+# Global kütüphaneler tüm projelerde kullanılabilir. Global yüklemek için bir sanal ortam (venv) aktif olmamalıdır.
+# powershell : deactivate
+# pip install pandas
+# pip show pandas
+# pip list
+
+
+# local kütüphane
+# Yerel yüklemeler genellikle sanal bir ortam (virtual environment) içinde yapılır.
+# Bu, yalnızca o proje için geçerli olan bir kütüphane ortamı sağlar.
+# powershell aç
+# python -m venv venv # Sanal Ortam Oluşturma:
+# venv\Scripts\activate    # Windows:
+# source venv/bin/activate # MacOS/Linux:
+# pip install pandas
+# pip show pandas
+# pip list
+# pip list --local
+
+
+################################################################
 # Bağımlılıkları Yükleme
 #pip install Flask
 #pip install matplotlib
