54745aa6e1187377c773f14f776c2c8d91f282c7
Hamit Mizrak <hamitmizrak@gmail.com>
2025-01-14 15:08:06 +0300

    Operators

---
+++ _04_operator.py
@@ -0,0 +1,61 @@
+################################################################
+# Operators
+"""
+1-) Aritmetik Operators
+    a) Toplama: +
+    b) Çıkarma: -
+    c) Çarpma: *
+    d) Bölme: /
+    e) Mod: %
+    f) Üs Alma: **
+    g) Kalan Sayı Bulma: //
+
+
+2-) Atama Operators
+    a) Eşittir: =
+    b) Artı Eşittir: +=
+    c) Çıkar Eşittir: -=
+    d) Bölme Eşittir: /=
+    e) Mod Eşittir: %=
+    f) Üs Alma Eşittir: **=
+    g) Kalan Sayı Bulma Eşittir: //=
+
+3-) Karşılaştırma Operators
+    a) Eşittir: ==
+    b) Eşit Değil: !=
+    c) Büyük Eşittir: >=
+    d) Büyük Değil: <=
+    e) Küçük Eşittir: <
+
+4-) Mantıksal Operators
+    a) VEYA: or
+    b) VE: and
+    c) Değil: not
+
+5-) Bit Düzeyinde (Bitwise) Operators  (Bit Düzeyinde: 0 ve 1 olanlardır)
+    a) Bitwise AND: &
+    b) Bitwise OR: |
+    c) Bitwise XOR: ^
+    d) Bitwise NOT: ~
+    e) Bitwise Left Shift: <<
+    f) Bitwise Right Shift: >>
+    g) Bitwise Right Shift (unsigned): >>
+
+
+5-) Atanma ve İfadeler
+    a) İfadeleri Birleştirir: +, -, *, /, %, **, //, ==,!=, >, <, >=, <=, or, and, not
+    b) Atama İfadesi: =, +=, -=, *=, /=, %=, **=, //=
+    c) İfadeleri Öncelik Belirlemek için Parantezler: ()
+    d) İfadeleri Birleştirirken İç içe kullanmak için Parantezler: ()
+    e) İfadeleri Birleştirirken Boşluklarla veya İfadeler arasında Boşluklar: ()
+    f) İfadeleri Birleştirirken İfadeler arasında ��arpanlar: ()
+    g) İfadeleri Birleştirirken İfadeler arasında Kullanılan Operatörler: +, -, *, /, %, **, //, ==,!=, >, <, >=, <=, or, and, not
+
+6-) Üyelik Operatör
+    a) İçinde İçe Kullanılan İfadeler: in
+    b) İçinde İçe Kullanılmamış İfadeler: not in
+    c) İçinde İçe Kullanılan İfadeler: is
+
+7-) Kimlik Operators
+    a) Kopyalanan İfadeler: =
+"""
