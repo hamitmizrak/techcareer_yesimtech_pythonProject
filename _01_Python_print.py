7675af739bca910003f0b0f40d9a5cf6f5331349
Hamit Mizrak <hamitmizrak@gmail.com>
2025-01-14 13:39:10 +0300

    print

---
+++ _01_Python_print.py
@@ -0,0 +1,48 @@
+# Single Comment
+
+"""
+Multiple Comment (Docstring)
+"""
+
+##################################################
+#### print #######################################
+# string
+print("Python Öğreniyorum")
+
+# Tam sayı
+print(44)
+
+# Virgüllü sayı
+print(44.23)
+
+# Birden fazla değer yazdırma
+print("Merhaba","Python","Öğreniyorum")
+
+# sep parameter: Ayraç ekle
+print("Merhaba", "Python", "Öğreniyorum", sep=" * ")
+
+# end parameter: Yeni bir satıra geçmesini engellesin
+print("Merhaba", "Python", "Öğreniyorum"," *** ","Dünya'ya hoşgeldin") # failed: non best practice
+print("Merhaba", "Python", "Öğreniyorum ", end=" *** ")
+print("Dünya'ya hoşgeldin")
+
+# docstring
+print( """docstring 
+    Python
+    Öğreniyorum
+    """)
+
+# Escape Character
+print("""\n\r\tdocstring 
+    Python
+    Öğreniyorum
+    """)
+
+# Değişken yazdırma
+isim = "Hamit"
+soyisim = "Mızrak"
+print("Adım:", isim," Soyadım: ", soyisim)    # 1.YOL
+print("Adım: %s, Soyadım:%s"%(isim,soyisim))  # 2.YOL  %s:string %d:decimal %f:virgüllü
+print(f"Adım: {isim}, Soyadım:  {soyisim}") # 3.YOL Formatter (Python>=3.6)
+
+
